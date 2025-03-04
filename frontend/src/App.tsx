91f3691348aa543a1fbd63cdfe56eddfeae0a0a8
KimYoungSoo <zerowater4704@gmail.com>
2025-03-04 21:06:31 +0900

    docker prod nginx 作成

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,9 +1,4 @@
-import {
-  BrowserRouter as Router,
-  Route,
-  Routes,
-  Navigate,
-} from "react-router-dom";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import Home from "./Home";
 import DashBoard from "./DashBoard";
 
